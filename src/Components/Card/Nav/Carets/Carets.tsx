5452f54c81b406d0e682f2ba638fccda601679a8
franklyT <48483484+franklyT@users.noreply.github.com>
2021-03-30 01:52:53 -0400

    css refac

--- src/Components/Card/Nav/Carets/Carets.tsx
+++ src/Components/Card/Nav/Carets/Carets.tsx
@@ -5,22 +5,22 @@ function Carets(props: any) {
     return (
       <div className={styles.containerRow}>
         <i
-            className={`${styles.caretStyle} ${styles.portfolioCaretStyle}`}
+            className={`${styles.caretStyle} ${styles.caretStylePortfolio}`}
             style={ props.parentState.activeCard === props.parentState.portfolioRef ? { height: "0px" } : { height: "100px" }}
         />
 
         <i
-            className={`${styles.caretStyle} ${styles.blogCaretStyle}`}
+            className={`${styles.caretStyle} ${styles.caretStyleBlog}`}
             style={ props.parentState.activeCard === props.parentState.blogRef ? { height: "0px" } : { height: "100px" }}
         />
 
         <i
-            className={`${styles.caretStyle} ${styles.codeCaretStyle}`}
+            className={`${styles.caretStyle} ${styles.caretStyleCode}`}
             style={ props.parentState.activeCard === props.parentState.codeRef ? { height: "0px" } : { height: "100px" }}
         />
 
         <i
-            className={`${styles.caretStyle} ${styles.atCaretStyle}`}
+            className={`${styles.caretStyle} ${styles.caretStyleAt}`}
             style={ props.parentState.activeCard === props.parentState.atRef ? { height: "0px" } : { height: "100px" }}
         />
       </div>
