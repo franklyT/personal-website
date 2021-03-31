0e4eff0ac3a87ec3a0ca3993ca988f01d24ed908
franklyT <48483484+franklyT@users.noreply.github.com>
2021-03-31 02:39:43 -0400

    Update HighlightSyntax.tsx

--- src/Components/Card/Nav/Contents/Meta/HighlightSyntax/HighlightSyntax.tsx
+++ src/Components/Card/Nav/Contents/Meta/HighlightSyntax/HighlightSyntax.tsx
@@ -3,10 +3,10 @@ import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 // materialDark
 // okaidia
 // tomorrow
-// ghcolors <- border: none;
+// ghcolors
 import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-function HighlightSyntax(props: any) {
+function HighlightSyntax(props: {code: string} ) {
     return (
         <React.Fragment>
             <SyntaxHighlighter language="jsx" style={materialDark} customStyle={{border: 'none', fontSize: '1.1em'}}>
