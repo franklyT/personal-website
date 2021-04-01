a0f31ae33b55bfeee612eb5df96fa7572cf096a4
franklyT <48483484+franklyT@users.noreply.github.com>
2021-04-01 02:55:41 -0400

    refactor

--- src/Components/Card/Nav/Contents/Cards/Blog/Pages/Entries/dinosaurTimes.tsx
+++ src/Components/Card/Nav/Contents/Cards/Blog/Pages/Entries/dinosaurTimes.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import {Entry} from "./Interface/Entry";
 
 export const dinosaurTimes: Entry = {
     date: "01/01/00",
     time: "1:00 AM",
     title: "A Treatise on the Brachiosaurus",
-    content: <React.Fragment> Brachiosaurus </React.Fragment>,
+    content: <> Brachiosaurus </>,
 }
--- src/Components/Card/Nav/Contents/Cards/Blog/Pages/Entries/shakespeare.tsx
+++ src/Components/Card/Nav/Contents/Cards/Blog/Pages/Entries/shakespeare.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import {Entry} from "./Interface/Entry";
 
 export const shakespeare: Entry = {
     date: "03/28/21",
     time: "10:30 PM",
     title: "Shakespeare, Fact or Fiction?",
-    content: <React.Fragment> SHAKESPEARE </React.Fragment>,
+    content: <> SHAKESPEARE </>,
 }
