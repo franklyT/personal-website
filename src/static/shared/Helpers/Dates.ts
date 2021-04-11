b65c2cbca7a400fa7bd0270c85d9a428e5c79e48
franklyT <48483484+franklyT@users.noreply.github.com>
2021-04-11 00:15:55 -0400

    refac date pad start

--- src/static/shared/Helpers/Dates.ts
+++ src/static/shared/Helpers/Dates.ts
@@ -1,2 +1,5 @@
 export const sanitizeDate = (date: string) => Number(date.replaceAll("/", ''));
-export const getTruncatedMonthDay = (date: string) => `${["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][Number(date.substring(0, 2)) - 1] || "Jan"} ${Number(date.substring(3, 5)) || "1"}`;
+export const getTruncatedMonthDay = (date: string) =>
+    `${["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        [Number(date.substring(0, 2)) - 1] || "Jan"} 
+        ${date.substring(3, 5).padStart(2, '0')}`;
