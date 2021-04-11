96135f0e5d90fac8a6ca9b6d352b1bf33a913876
franklyT <48483484+franklyT@users.noreply.github.com>
2021-04-11 15:56:02 -0400

    more blog refactoring

--- src/Components/Card/Page/Blog/Categories/Post/Post.tsx
+++ src/Components/Card/Page/Blog/Categories/Post/Post.tsx
@@ -2,20 +2,26 @@ import {useState} from 'react';
 import ReactMarkdown from "react-markdown";
 import {getTruncatedMonthDay} from "../../../../../../static/shared/Helpers/Dates";
 
+import styles from './Post.module.scss';
+
 export function Post(props: any) {
     const [isPostRendered, setIsPostRendered] = useState(false);
     const {page} = props.propObj;
 
     return (
-        <div style={{overflowY: "hidden"}}
-             onClick={() => isPostRendered ? setIsPostRendered(false) : setIsPostRendered(true)}>
-            <div style={{display: 'flex', flexDirection: "row", alignItems: "center"}}>
-                <p style={{fontSize: '0.75rem', color: "gray", marginRight: "2rem", whiteSpace: "nowrap"}}>
+        <div className={styles.container}>
+            <div className={styles.flexContainer}
+                 onClick={() => isPostRendered ? setIsPostRendered(false) : setIsPostRendered(true)}>
+                <p className={styles.monthDay}>
                     {getTruncatedMonthDay(page.date)}
                 </p>
 
-                <p>
+                <p className={styles.title}>
                     {page.title}
+                </p>
+
+                <p className={`${styles.chevron} ${isPostRendered ? styles.rotated : ''}`} >
+                    &gt;
                 </p>
             </div>
             <ReactMarkdown children={page.markdown} className={isPostRendered ? "expand" : "collapse"}/>
